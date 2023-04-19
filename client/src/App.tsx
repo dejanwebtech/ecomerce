fb6e429a1a453f5a0bbaa71ce2d687f4c5785bd5
dejanwebtech <dejanmatic.dev@gmail.com>
2023-04-19 22:50:36 +0200

    added navbar and card placement

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,44 @@
-import { useState } from "react";
 import { sampleProducts } from "./data";
+import { Navbar, Container, Nav, Row, Col } from "react-bootstrap";
 
 function App() {
     return (
-        <div>
-            <header>eCom</header>
+        <div className="d-flex flex-column vh-100">
+            <header>
+                <Navbar bg="dark" variant="dark" expand="lg">
+                    <Container>
+                        <Navbar.Brand>eCom</Navbar.Brand>
+                    </Container>
+                    <Nav>
+                        <a href="/cart" className="nav-link">
+                            Cart
+                        </a>
+                        <a href="/signin" className="nav-link">
+                            Sign In
+                        </a>
+                    </Nav>
+                </Navbar>
+            </header>
             <main>
-                <ul>
-                    {sampleProducts.map((product) => (
-                        <li key={product.slug}>
-                            <img
-                                src={product.image}
-                                alt={product.name}
-                                className="product-image"
-                            />
-                            <h2>{product.name}</h2>
-                            <p>${product.price}</p>
-                        </li>
-                    ))}
-                </ul>
+                <Container className="mt-3">
+                    <Row>
+                        {sampleProducts.map((product) => (
+                            <Col key={product.slug} sm={6} md={4} lg={3}>
+                                <img
+                                    src={product.image}
+                                    alt={product.name}
+                                    className="product-image"
+                                />
+                                <h2>{product.name}</h2>
+                                <p>${product.price}</p>
+                            </Col>
+                        ))}
+                    </Row>
+                </Container>
             </main>
-            <footer>All rights reserved</footer>
+            <footer>
+                <div className="text-center">All rights reserved</div>
+            </footer>
         </div>
     );
 }
