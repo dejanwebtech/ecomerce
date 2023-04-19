ca8ac8cd207adb31bdecbe5511e7b8dc9fbda790
dejanwebtech <dejanmatic.dev@gmail.com>
2023-04-19 21:34:26 +0200

    init client

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+function App() {
+    return (
+        <div>
+            <header>eCom</header>
+            <main></main>
+            <footer>All rights reserved</footer>
+        </div>
+    );
+}
+
+export default App;
